cb8e0c4156e6ee6057a9182acad5d158a01e84ce
Pierre Tassel <pierre.tassel@outlook.fr>
2020-02-06 14:23:56 +0100

    #17 sorting flight bug + solution cost in the incremental approach

--- instance_generator/route_gen.py
+++ instance_generator/route_gen.py
@@ -439,7 +439,7 @@ def asp_input_fact(output_file, solution):
     # we put the generated instance inside the ../instances folder
     file = open(
         str(pathlib.Path(__file__).parent.parent) + "/instances/" +
-        output_file + ".lp", "w")
+        output_file + ".lp", "w+")
     file.write(repr(solution))
     file.close()
 
