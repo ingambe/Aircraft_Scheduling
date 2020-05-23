1f7325f6f0134b1d44072d58557e8779bfe640c7
Pierre TASSEL <pierre.tassel@outlook.fr>
2020-05-23 11:18:19 +0200

    #34 plot first benchmark

--- benchmarks/results/draw_plot.py
+++ benchmarks/results/draw_plot.py
@@ -14,11 +14,13 @@ for i, encoding in enumerate(encodings):
     color_encoding[encoding] = colors[i]
 
 
-for i in range(1):
+for i in range(10):
     for encoding in encodings:
         x = []
         y = []
         dirFiles = os.listdir("{}/".format(encoding))
+        # we remove hidden file like ".gitkeep"
+        dirFiles = [f for f in dirFiles if not f.startswith('.')]
         dirFiles.sort()
         data = pd.read_csv("{}/{}".format(encoding, dirFiles[i]))
         for row in data.itertuples():
@@ -39,13 +41,12 @@ running_time_result = [encoding for encoding in os.listdir('.') if encoding.ends
 for result in running_time_result:
     plt.clf()
     df = pd.read_csv("{}".format(result))
-    print(df)
     # we remove column with inf or with overtime
     df = df.replace([np.inf, sys.maxsize], np.nan)
     df = df.dropna(axis=1)
     # we need also to remove the unnamed column
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-    sns.violinplot(data=df, cut=0)
+    sns.violinplot(data=df, cut=0, scale='width')
     plt.xlabel("Encoding")
     if "cost" in result:
         plt.ylabel("Solution Cost")
